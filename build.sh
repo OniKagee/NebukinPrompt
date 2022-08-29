8b1c31e841bdcf07abb292717c60199a912699fb
NebularPetrov <69680236+NebularPetrov@users.noreply.github.com>
2022-08-29 13:41:53 +0300

    ok, let's go!

--- build.sh
+++ build.sh
@@ -11,6 +11,9 @@ if test "`whoami`" != "root" ; then
 	echo "You must be logged in as root to build (for loopback mounting)"
 	echo "Enter 'su' or 'sudo bash' to switch to root"
 	exit
+else
+	echo "root found!"
+	sleep 5
 fi
 
 
