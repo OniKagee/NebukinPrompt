ecb61824695982acf72bb0a0c788fa6769f7bcb9
Petr S. Nebukin <pnebookin@bk.ru>
2022-09-28 18:46:18 +0300

    Update #3728.000168 for NPF Viewer

--- programs/npfview.asm
+++ programs/npfview.asm
@@ -1,5 +1,5 @@
 ; ------------------------------------------------------------------
-; Program to display text files and PCX images (320x200, 8-bit only)
+; Program to display NPF (320x200, 8-bit only)
 ; ------------------------------------------------------------------
 
 
@@ -26,24 +26,10 @@ main_start:
 	dec di
 	dec di				; ...and now to first char of extension!
 
-	mov si, txt_extension
+	mov si, npf_extension
 	mov cx, 3
-	rep cmpsb			; Does the extension contain 'TXT'?
-	je near valid_txt_extension	; Skip ahead if so
-
-	dec di
-
-	mov si, bas_extension
-	mov cx, 3
-	rep cmpsb			; Does the extension contain 'BAS'?
-	je near valid_txt_extension	; Skip ahead if so
-
-	dec di
-
-	mov si, pcx_extension
-	mov cx, 3
-	rep cmpsb			; Does the extension contain 'PCX'?
-	je valid_pcx_extension		; Skip ahead if so
+	rep cmpsb			; Does the extension contain 'NPF'?
+	je valid_npf_extension		; Skip ahead if so
 
 					; Otherwise show error dialog
 	mov dx, 0			; One button for dialog box
@@ -55,9 +41,9 @@ main_start:
 	jmp main_start			; And retry
 
 
-valid_pcx_extension:
+valid_npf_extension:
 	mov ax, bx
-	mov cx, 36864			; Load PCX at 36864 (4K after program start)
+	mov cx, 36864			; Load NPF at 36864 (4K after program start)
 	call os_load_file
 
 
@@ -119,58 +105,9 @@ setpal:
 draw_background:
 	mov ax, title_msg		; Set up screen
 	mov bx, footer_msg
-	mov cx, BLACK_ON_WHITE
+	mov cx, WHITE_ON_GREEN
 	call os_draw_background
 	ret
-
-
-
-	; Meanwhile, if it's a text file...
-
-valid_txt_extension:
-	mov ax, bx
-	mov cx, 36864			; Load file 4K after program start
-	call os_load_file
-
-
-	; Now BX contains the number of bytes in the file, so let's add
-	; the load offset to get the last byte of the file in RAM
-
-	add bx, 36864
-
-
-	mov cx, 0			; Lines to skip when rendering
-	mov word [skiplines], 0
-
-
-	pusha
-	mov ax, txt_title_msg		; Set up screen
-	mov bx, txt_footer_msg
-	mov cx, 11110000b		; Black text on white background
-	call os_draw_background
-	popa
-
-
-
-txt_start:
-	pusha
-
-	mov bl, 11110000b		; Black text on white background
-	mov dh, 2
-	mov dl, 0
-	mov si, 80
-	mov di, 23
-	call os_draw_block		; Overwrite old text for scrolling
-
-	mov dh, 2			; Move cursor to near top
-	mov dl, 0
-	call os_move_cursor
-
-	popa
-
-
-	mov si, 36864			; Start of text data
-	mov ah, 0Eh			; BIOS char printing routine
 
 
 redraw:
@@ -207,59 +144,17 @@ skip_return:
 	jmp loopy
 
 
-get_input:				; Get cursor keys and Q
-	call os_wait_for_key
-	cmp ah, KEY_UP
-	je go_up
-	cmp ah, KEY_DOWN
-	je go_down
-	cmp al, 'q'
-	je main_start
-	cmp al, 'Q'
-	je main_start
-	jmp get_input
-
-
-go_up:
-	cmp word [skiplines], 0		; Don't scroll up if we're at the top
-	jle txt_start
-	dec word [skiplines]		; Otherwise decrement the lines we need to skip
-	mov word cx, [skiplines]
-	jmp txt_start
-
-go_down:
-	inc word [skiplines]		; Increment the lines we need to skip
-	mov word cx, [skiplines]
-	jmp txt_start
-
-
-finished:				; We get here when we've printed the final character
-	call os_wait_for_key
-	cmp ah, 48h
-	je go_up			; Can only scroll up at this point
-	cmp al, 'q'
-	je main_start
-	cmp al, 'Q'
-	je main_start
-	jmp finished
-
-
 close:
 	call os_clear_screen
 	ret
 
 
-	txt_extension	db 'TXT', 0
-	bas_extension	db 'BAS', 0
-	pcx_extension	db 'PCX', 0
+	npf_extension	db 'NPF', 0
 
-	err_string	db 'Please select a TXT, BAS or PCX file!', 0
+	err_string	db 'Please select a NPF file!', 0
 
-	title_msg	db 'Nebukin File Viewer', 0
-	footer_msg	db 'Select a TXT, BAS or PCX file to view, or press Esc to exit', 0
-
-	txt_title_msg	db 'Nebukin Text Viewer', 0
-	txt_footer_msg	db 'Use arrow keys to scroll and Q to quit', 0
+	title_msg	db 'Nebukin Presentation Viewer', 0
+	footer_msg	db 'Select a PPF file to view, or press Esc to exit', 0
 
 	skiplines	dw 0
 
